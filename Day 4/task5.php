86ebb2e814a8355e143b9e781f559fe4fdf8f05d
sakshamkadayat <sakshamkadayat@gmail.com>
2021-03-15 12:21:42 +0000

    Task completed

--- Day 4/task5.php
+++ Day 4/task5.php
@@ -1,3 +1,29 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tutorials</title>
+</head>
+<body>
+    
+
+
+
+
 <?php
 
+
 // create a varibale and store basic syntax of php in it. print the variable using echo and <code> tag of HTML
+$variable="codewithsudeep ";
+echo $variable;
+
+
+?>
+
+
+</body>
+</html>
